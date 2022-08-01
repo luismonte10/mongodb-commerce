0569d27ee6a0b310b4be661c9962eb4b7dfa43f5
Luis Monte <luismonte.l560@gmail.com>
2022-08-01 02:29:11 -0300

    feat: desafio 21

---
+++ challenges/desafio21.js
@@ -0,0 +1,9 @@
+db.produtos.updateOne(
+  { nome: { $eq: "Cheddar McMelt" } },
+  { $pop: { ingredientes: 1 } },
+);
+
+db.produtos.find(
+  {},
+  { _id: false, nome: true, ingredientes: true },
+);
